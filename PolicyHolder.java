83d564f85c24dd16407887709eb1078c604e2e33
Yadiel Collado <yacolladi@gmail.com>
2026-04-23 18:36:33 -0400

    Update PolicyHolder.java
    Imported fields and methods from Policy class

---
+++ PolicyHolder.java
@@ -0,0 +1,50 @@
+public class PolicyHolder {
+   private String firstName;
+   private String lastName;
+   private int age;
+   private String smokingStatus;
+   private double height;
+   private double weight;
+   
+   
+   public PolicyHolder() {
+      firstName = "";
+      lastName = "";
+      age = 0;
+      smokingStatus = "";
+      height = 0;
+      weight = 0;
+     }
+     
+     
+     public PolicyHolder(String firstName, String lastName, int age, String smokingStatus, double height, double weight) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.smokingStatus = smokingStatus;
+      this.height = height;
+      this.weight = weight;
+     }
+     
+     
+     public String getFirstName() {return firstName;}
+     public String getLastName() {return lastName;}
+     public int getAge() {return age;}
+     public String getSmokingStatus() {return smokingStatus;}
+     public double getHeight() {return height;}
+     public double getWeight() {return weight;}
+     
+     
+     public void setFirstName(String firstName) {this.firstName = firstName;}
+     public void setLastName(String lastName) {this.lastName = lastName;}
+     public void setAge(int age) {this.age = age;}
+     public void setSmokingStatus(String smokingStatus) {this.smokingStatus = smokingStatus;}
+     public void setHeight(double height) {this.height = height;}
+     public void setWeight(double weight) {this.weight = weight;}
+     
+     
+     public double calculateBMI() {
+      if(height == 0) return 0;
+      return(weight * 703) / (height * height);
+     }
+    }
